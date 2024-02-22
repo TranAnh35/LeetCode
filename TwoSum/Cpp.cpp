7cad0f96214eec4a54de1d87c84c4f83624333eb
TranAnh35 <tranleanh352004@gmail.com>
2024-02-22 22:37:53 +0700

    Day1

---
+++ TwoSum/Cpp.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(target - nums[i]) != map.end()) {
+                result.push_back(map[target - nums[i]]);
+                result.push_back(i);
+                return result;
+            }
+            map[nums[i]] = i;
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
