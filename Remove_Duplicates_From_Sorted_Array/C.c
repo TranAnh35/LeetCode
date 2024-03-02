5ee448d454e5930cc2cd687e397de3375fbb23e2
TranAnh35 <tranleanh352004@gmail.com>
2024-03-02 19:52:33 +0700

    Day10

---
+++ Remove_Duplicates_From_Sorted_Array/C.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize){
+    if(numsSize == 0) return 0;
+    int i = 0;
+    for(int j = 1; j < numsSize; j++){
+        if(nums[j] != nums[i]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i+1;
+}
+
+int main(){
+    int nums[] = {1, 1, 2};
+    int numsSize = 3;
+    int result = removeDuplicates(nums, numsSize);
+    printf("%d\n", result);
+    return 0;
+}
