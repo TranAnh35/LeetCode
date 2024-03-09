b44b5e5b7b0ba9328c633862762aae9b42add011
TranAnh35 <tranleanh352004@gmail.com>
2024-03-09 17:42:48 +0700

    Day16

---
+++ Valid_Parentheses/Py.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c == '(' or c == '[' or c == '{':
+                stack.append(c)
+            else:
+                if len(stack) == 0:
+                    return False
+                if c == ')' and stack[-1] != '(':
+                    return False
+                if c == ']' and stack[-1] != '[':
+                    return False
+                if c == '}' and stack[-1] != '{':
+                    return False
+                stack.pop()
+        return len(stack) == 0
+    
+    
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isValid("()"))
+    print(s.isValid("()[]{}"))
+    print(s.isValid("(]"))
+    print(s.isValid("([)]"))
+    print(s.isValid("{[]}"))
+    print(s.isValid("]"))
