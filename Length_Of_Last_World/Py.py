bd4fbc3caf3c547b0efea48e53c51f8c0c63bb6b
TranAnh35 <tranleanh352004@gmail.com>
2024-03-09 17:43:22 +0700

    Day16

--- Length_Of_Last_World/Py.py
+++ Length_Of_Last_World/Py.py
@@ -1,9 +1,6 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        if(len(s) == 0):
-            return 0
-        lst = s.split()
-        return len(lst[-1])
+        return len(s.split()[-1])
     
     
 if __name__ == "__main__":
